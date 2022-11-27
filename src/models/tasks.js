f590a2fac863d9848554daea3c424ae3987fa2b0
Midhun <iammidhunpj@gmail.com>
2022-11-27 14:49:33 +0530

    initial commit

---
+++ src/models/tasks.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const Task = mongoose.model('Task', {
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = Task;
